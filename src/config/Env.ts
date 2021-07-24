16231ad344ff3bdb390d8ff7ff252d62f8a3bb68
Raisess <raisechif@gmail.com>
2021-07-24 11:29:24 -0300

    Implemented env updates.

--- src/config/Env.ts
+++ src/config/Env.ts
@@ -2,4 +2,6 @@ import "dotenv/config";
 
 export default class Env {
   public static TOKEN: string = process.env.TOKEN!;
+  public static NAME: string = process.env.NAME || "Disc Bot";
+  public static PREFIX: string = process.env.PREFIX || "!"; 
 }
