9e5b8807ecedfd77a51f332266324b214b82ee1f
Raisess <raisechif@gmail.com>
2021-07-24 11:30:43 -0300

    prettier

--- src/config/Env.ts
+++ src/config/Env.ts
@@ -3,5 +3,5 @@ import "dotenv/config";
 export default class Env {
   public static TOKEN: string = process.env.TOKEN!;
   public static NAME: string = process.env.NAME || "Disc Bot";
-  public static PREFIX: string = process.env.PREFIX || "!"; 
+  public static PREFIX: string = process.env.PREFIX || "!";
 }
