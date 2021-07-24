bd2b678a74041b971c3a85218908c0641eecf55c
Raisess <raisechif@gmail.com>
2021-07-24 12:37:51 -0300

    Created hello command.

---
+++ src/commands/HelloCommand.ts
@@ -0,0 +1,12 @@
+import ICommand, { Command } from "./ICommand";
+
+export default class HelloCommand implements ICommand {
+  public readonly name: string = "hello";
+  public readonly description: string = "Hello command";
+
+  public execute(command: Command): void {
+    const author: string = command.message.author.username;
+
+    command.message.channel.send(`Hello, ${author}!`);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import Core from "./Core";
 import Env from "./config/Env";
 
-const bot: Core = new Core(Env.NAME, Env.PREFIX);
+import HelloCommand from "./commands/HelloCommand";
+
+const bot: Core = new Core(Env.NAME, Env.PREFIX, [new HelloCommand()]);
 
 bot.authClient(Env.TOKEN);
